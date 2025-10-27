067e8c58087483b500afbd78509faede03a67d99
Ali Örer <129732909+AliOrer@users.noreply.github.com>
2025-10-27 09:33:04 +0100

    Implement message cycling on button click
    yeni branch eklendi ve öncekinde tek bir metin yazarken, şimdi dizi ile yeni birden fazla metin yazabileceğiz

--- merhaba-dunya- butonu/main.js
+++ merhaba-dunya- butonu/main.js
@@ -3,6 +3,18 @@ let button = getElementById('helloBtn'); //htmldeki butonu buluyoruz
 
 let output = getElementById('output'); //yazının gösterileceği p etiketini buluyoruz
 
-button.addEventListener('click', function(){ //butona tıklanınca ne yapacağını söylüyoruz
-  output.textContent = "merhaba dünya!";
-});  //p etiketinin içine textContent ile yeni metini ekliyoruz
+let messages = ["Merhaba Dünya!", "Nasılsın?", "Hoş Geldin!"]; //gösterilecek metinleri array içine topluyoruz
+
+let index = 0; //hangi sırada olduğumuzu tutmak için bir sayaç eğkliyoruz
+
+
+
+button.addEventListener('click', function(){ //paragrafın içine  dizideki yazıyı koyuyoruz
+  output.textContent = messages[index];
+  
+  index = index +1; //sonraki yazıya geçmek için indexi 1 artırıyoruz
+  
+  if(index == messages.length) { //eger sona geldiysek basa dönderiyoruz
+    index = 0;
+  }
+});  
