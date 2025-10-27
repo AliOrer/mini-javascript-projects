d86145509010214f73da60e3366529b7ecdf6a88
Ali Örer <129732909+AliOrer@users.noreply.github.com>
2025-10-27 11:24:05 +0300

    KTÜKTD

--- merhaba-dunya- butonu/main.js
+++ merhaba-dunya- butonu/main.js
@@ -1,1 +1,8 @@
 
+let button = getElementById('helloBtn'); //htmldeki butonu buluyoruz
+
+let output = getElementById('output'); //yazının gösterileceği p etiketini buluyoruz
+
+button.addEventListener('click', function(){ //butona tıklanınca ne yapacağını söylüyoruz
+  output.textContent = "merhaba dünya!";
+});  //p etiketinin içine textContent ile yeni metini ekliyoruz
