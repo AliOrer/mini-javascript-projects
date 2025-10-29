03a5b5176eb3996d8c2c4b5a0ce5beb8b11cd6ed
Ali Örer <nemetosce@gmail.com>
2025-10-30 00:25:39 +0300

    Add input validation for price range slider
    Introduced event listeners to validate minimum price input in the price range slider. Now alerts the user and resets the value if the minimum price is set below zero.

--- price-range-slider/main.js
+++ price-range-slider/main.js
@@ -1,1 +1,23 @@
 
+const rangeValue = document.querySelector(".slider .price-slider");
+const rangeInputValue = document.querySelector(".range-input input");
+
+let priceGap = 500; //set the price gap
+
+const priceInputValue = document.querySelectorAll(" .price-input input");
+
+for (let i=0; i< priceInputValue.length; i++){
+    priceInputValue[i].addEventListener("input", e =>{
+        //parse min - max values of the range list
+        let minp = parseInt(priceInputValue[0].value);
+        let maxp = parseInt(priceInputValue[1].value);
+        let diff = maxp-minp
+        
+
+        if(minp < 0){
+            alert("minimum price cannot be less than 0");
+            priceInputValue[0].value = 0;
+            minp = 0;
+        }
+    })
+}
